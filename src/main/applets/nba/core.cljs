dbc9bb7d7402b6924a17dce3dd48ff7ec34e7aa3
Chris Murphy <chris@strandz.org>
2018-08-15 03:32:49 +0800

    Some explanation in the client REPL

--- src/main/applets/nba/core.cljs
+++ src/main/applets/nba/core.cljs
@@ -8,21 +8,21 @@
             [applets.nba.operations :as ops]
             [general.dev :as dev]))
 
-(defonce app (atom nil))
+(defonce app-atom (atom nil))
 
 (defn mount [comp]
-  (reset! app (fc/mount @app comp "app")))
+  (reset! app-atom (fc/mount @app-atom comp "app")))
 
 (defn ^:export init [{:keys [stage] :as opts}]
-  (reset! app (fc/new-fulcro-client
-                :started-callback (fn [app]
-                                    (let [rec (:reconciler app)]
-                                      (js/setTimeout #(do (common/load-nba-games! common/PlayerYears
-                                                                                  opts
-                                                                                  constants/desired-labels
-                                                                                  rec)
-                                                          (prim/transact! rec `[(ops/fill-desired-labels)]))
-                                                     50)))))
+  (reset! app-atom (fc/new-fulcro-client
+                     :started-callback (fn [app]
+                                         (let [rec (:reconciler app)]
+                                           (js/setTimeout #(do (common/load-nba-games! common/PlayerYears
+                                                                                       opts
+                                                                                       constants/desired-labels
+                                                                                       rec)
+                                                               (prim/transact! rec `[(ops/fill-desired-labels)]))
+                                                          50)))))
   (mount (cond
            (= stage :naive) naive-root/NBARoot
            (#{:nyt :improved} stage) non-naive-root/NBARoot
--- src/main/general/user.cljs
+++ src/main/general/user.cljs
@@ -2,6 +2,12 @@
   (:require [general.dev :as dev]
             [fulcro.client.primitives :as prim]))
 
+;;
+;; Note that this has not been set up. Fulcro Inspect will probably be all you need.
+;; I've only ever needed this browser REPL to get a copy of state, something that FI can't do.
+;; One way to make this ns serviceable would be to delete the applets.nba.core/app-atom, and
+;; instead use this app-atom from applets.nba.core
+;;
 (def app-atom (atom nil))
 
 (defn get-state []
